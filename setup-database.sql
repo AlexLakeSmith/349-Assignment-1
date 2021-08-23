2b60a17eef6e68a59647722dfcf56552a107e8dd
Alex Lake-Smith <alexanderlakesmith@gmail.com>
2021-08-24 11:30:49 +1200

    Single VM example of PHP connecting to a MySQL database

---
+++ setup-database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE papers (
+  code varchar(7),
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (code)
+);
+
+INSERT INTO papers VALUES ('COSC326','Effective Programming');
+INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture')
+INSERT INTO papers VALUES ('COSC301','Network Management and Security')
+INSERT INTO papers VALUES ('COSC343','Artificial Intelligence')
+;
